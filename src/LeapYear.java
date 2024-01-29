58a8dfd3bd9d300ebbf9529568f7891ebc93789f
Daria Orlova <da.orlova365@gmail.com>
2024-01-29 05:06:47 +0300

    Completed

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -5,10 +5,10 @@ public class LeapYear {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите год: ");
         int year = in.nextInt();
-        if (year>=1584) {
-            if ((year%400==0)||(year%4==0)||(year%100!=0)){
+        if (year >= 1584) {
+            if ((year % 400 == 0) || (year % 4 == 0) || (year % 100 != 0)) {
                 System.out.println("Год високосный");
-            }else {
+            } else {
                 System.out.println("Год невисокосный");
             }
         } else {
