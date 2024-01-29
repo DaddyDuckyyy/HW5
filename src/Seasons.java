bca6b3fdd530b4ab8d5f60eb24a7d0daaa431d3d
Daria Orlova <da.orlova365@gmail.com>
2024-01-29 04:55:41 +0300

    Completed

--- src/Seasons.java
+++ src/Seasons.java
@@ -5,7 +5,13 @@ public class Seasons {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите номер месяца: ");
         int monthNumber = in.nextInt();
-
+        switch (monthNumber) {
+            case 1, 2, 12 -> System.out.println("Зима");
+            case 3, 4, 5 -> System.out.println("Весна");
+            case 6, 7, 8 -> System.out.println("Лето");
+            case 9, 10, 11 -> System.out.println("Осень");
+            default -> System.out.println("Неправильный номер");
+        }
         in.close();
     }
 }
